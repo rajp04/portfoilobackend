bc336520082eb547d8c5071927ae342562fd2a50
Raj Ghodasara <127863376+rajp04@users.noreply.github.com>
2023-08-23 23:42:43 +0530

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 require('./DB/db')
 app.use(express.json())
