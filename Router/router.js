b0e603e30aee8cc8038e243857584ac0bfd868de
Raj Ghodasara <127863376+rajp04@users.noreply.github.com>
2023-08-24 00:28:53 +0530

    serven commit

--- Router/router.js
+++ Router/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const user = require('../models/user')
 
-router.post('/', async (req, res) => {
+router.post('/contact', async (req, res) => {
     
     const { name, email, message } = req.body;
     if (!name || !email || !message) { 
